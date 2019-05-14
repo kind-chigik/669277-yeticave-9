394651a4757d43a480d89925028999cc5e7824bb
kind-chigik <rus.work89@yandex.ru>
2019-05-14 20:43:14 +0300

    Добавлено подключение к БД, реализован вывод лотов и категории через БД

--- index.php
+++ index.php
@@ -1,50 +1,29 @@
 <?php
 require_once('helpers.php');
 
+$bd = mysqli_connect('localhost', 'root', '', 'yeticave');
+mysqli_set_charset($bd, 'utf8');
+
+if (!$bd) {
+    print('Ошибка подключения: ' . mysqli_connect_error());
+};
+
+$sql_category = 'SELECT *'
+    . 'FROM category';
+$result_category = mysqli_query($bd, $sql_category);
+$categories = mysqli_fetch_all($result_category, MYSQLI_ASSOC);
+
+$sql_lot = 'SELECT l.id, l.name, start_price, image, category_id, MAX(r.amount) '
+    . 'FROM lot l '
+    . 'LEFT JOIN category c ON category_id = c.id '
+    . 'LEFT JOIN rate r ON r.lot_id = l.id '
+    . 'WHERE end_time > CURRENT_TIMESTAMP '
+    . 'GROUP BY l.id ORDER BY l.creation_time DESC ';
+$result_lot = mysqli_query($bd, $sql_lot);
+$lots = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
+
 $is_auth = rand(0, 1);
-
 $user_name = 'Inna'; // укажите здесь ваше имя
-
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-
-$lots = [
-    [
-        'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
-        'image' => 'img/lot-1.jpg'
-    ],
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
-        'image' => 'img/lot-2.jpg'
-    ],
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        "category" => 'Крепления',
-        'price' => '8000',
-        'image' => 'img/lot-3.jpg'
-    ],
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
-        'image' => 'img/lot-4.jpg'
-    ],
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
-        'image' => 'img/lot-5.jpg'
-    ],
-    [
-        'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
-        'image' => 'img/lot-6.jpg'
-    ]
-];
 
 $page_content = include_template('index.php', [
     'categories' => $categories,
