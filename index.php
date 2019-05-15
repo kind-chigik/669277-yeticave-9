41ef72469defc4abed0b56531f7f6631f1ce38da
kind-chigik <rus.work89@yandex.ru>
2019-05-15 03:02:36 +0300

    Реализовано подлючение к БД через функции

--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <?php
 require_once('helpers.php');
+$config = require 'config.php';
 
-$bd = mysqli_connect('localhost', 'root', '', 'yeticave');
-mysqli_set_charset($bd, 'utf8');
+$connection = bd_connect($config['bd']);
 
-if (!$bd) {
-    print('Ошибка подключения: ' . mysqli_connect_error());
-};
+$categories = getCategories($connection);
 
-$sql_category = 'SELECT *'
-    . 'FROM category';
-$result_category = mysqli_query($bd, $sql_category);
-$categories = mysqli_fetch_all($result_category, MYSQLI_ASSOC);
-
-$sql_lot = 'SELECT l.id, l.name, start_price, image, category_id, MAX(r.amount) '
-    . 'FROM lot l '
-    . 'LEFT JOIN category c ON category_id = c.id '
-    . 'LEFT JOIN rate r ON r.lot_id = l.id '
-    . 'WHERE end_time > CURRENT_TIMESTAMP '
-    . 'GROUP BY l.id ORDER BY l.creation_time DESC ';
-$result_lot = mysqli_query($bd, $sql_lot);
-$lots = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
+$lots = getLots($connection);
 
 $is_auth = rand(0, 1);
 $user_name = 'Inna'; // укажите здесь ваше имя
