2728dfb9ed926afa36fa3120658c6d5c40e532c7
kind-chigik <rus.work89@yandex.ru>
2019-06-03 00:24:46 +0300

    Добавлена переменная, которая хранит id пользователя

--- init.php
+++ init.php
@@ -3,3 +3,4 @@ session_start();
 
 $is_auth = isset($_SESSION['user']) ? true : false;
 $user_name = $is_auth ? strip_tags($_SESSION['user']['name']) : false;
+$user_id = $is_auth ? htmlspecialchars($_SESSION['user']['id']) : false;
