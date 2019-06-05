1add4f743dd19455958ec70cfc29eed6b0606c74
kind-chigik <rus.work89@yandex.ru>
2019-06-05 18:45:19 +0300

    Добавлено описание к функциями

--- helpers.php
+++ helpers.php
@@ -3,6 +3,11 @@ const RUB = '<b class="rub">р</b>';
 const HOUR = 3600;
 const MINUTE = 60;
 
+/**
+ * Округляет число в большую сторону и добавляет пробел после тысячных
+ * @param float $amount число, которое нужно отформатировать
+ * @return float оформатированное число
+ */
 function formatting_amount($amount)
 {
     $amount = ceil($amount);
@@ -11,8 +16,6 @@ function formatting_amount($amount)
     }
     return $amount . RUB;
 }
-
-;
 
 /**
  * Проверяет переданную дату на соответствие формату 'ГГГГ-ММ-ДД'
@@ -163,6 +166,11 @@ function include_template($name, array $data = [])
     return $result;
 }
 
+/**
+ * Определяет разницу между текущим временем и определенной датой, передает туда дату и возвращает оформитированное время
+ * @param string $end_time дата, для которой нужно посчитать разницу во времени
+ * @return string Итоговая отформатированная дата
+ */
 function time_before_end(string $end_time)
 {
     $end_time = strtotime($end_time);
@@ -174,6 +182,11 @@ function time_before_end(string $end_time)
     return $formatting_time;
 }
 
+/**
+ * Определяет разницу между текущим временем и определенной датой, передает туда дату и возвращает true или false
+ * @param string $end_time дата, для которой нужно посчитать разницу во времени
+ * @return bool логическое значение
+ */
 function less_hour_left($end_time)
 {
     $end_time = strtotime($end_time);
@@ -184,6 +197,11 @@ function less_hour_left($end_time)
     return true;
 }
 
+/**
+ * Устанавливает подлючение к БД, передает туда данные для подключения, возвращает результат подключения
+ * @param array $db_config массив, в котором хранятся настройки подлючения
+ * @return mysqli результат подключения
+ */
 function db_connect(array $db_config): mysqli
 {
     $connection = mysqli_connect($db_config['host'], $db_config['user'], $db_config['password'],
@@ -195,6 +213,11 @@ function db_connect(array $db_config): mysqli
     return $connection;
 }
 
+/**
+ * Получает все категории из БД
+ * @param mysqli $connection ресурс соединения с БД
+ * @return array массив с категориями
+ */
 function get_categories($connection)
 {
     $sql_category = 'SELECT * FROM category';
@@ -203,6 +226,11 @@ function get_categories($connection)
     return $categories;
 }
 
+/**
+ * Получает все лоты из БД
+ * @param mysqli $connection ресурс соединения с БД
+ * @return array массив с лотами
+ */
 function get_lots($connection)
 {
     $sql_lot = 'SELECT l.id, l.name, start_price, image, category_id, MAX(r.amount), l.end_time '
@@ -215,20 +243,33 @@ function get_lots($connection)
     return $lots;
 }
 
+/**
+ * Получает одну запись из результата запроса в БД в виде массива
+ * @param mysqli $connection ресурс соединения с БД
+ * @return array массив с данными, либо ошибка, если запрос не выполнился успешно
+ */
 function get_row_from_mysql($connection, $sql)
 {
     $result = mysqli_query($connection, $sql);
     return ($result) ? mysqli_fetch_assoc($result) : die("Ошибка " . mysqli_error($connection));
 }
 
-;
-
+/**
+ * Получает двумерные массив из результата запроса в БД
+ * @param mysqli $connection ресурс соединения с БД
+ * @return array двумерный массив с данными, либо ошибка, если запрос не выполнился успешно
+ */
 function get_rows_from_mysql($connection, $sql)
 {
     $result = mysqli_query($connection, $sql);
     return ($result) ? mysqli_fetch_all($result, MYSQLI_ASSOC) : die("Ошибка " . mysqli_error($connection));
 }
 
+/**
+ * Определяет разницу между текущим временем и определенной датой, передает туда дату и возвращает метку времени в формате Unixtime
+ * @param string $end_time дата, для которой нужно посчитать разницу во времени и метку времени
+ * @return string метка времени Unixtime
+ */
 function count_time($date)
 {
     return strtotime($date) - time();
